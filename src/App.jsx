2276f4d8a6bded6c5994d7a343623994290736f4
edlance3 <edlance3@gmail.com>
2022-12-18 18:30:13 -0500

    Road to React - List

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,48 @@
 import * as React from 'react';
- 
-//const title = 'React!';
 
-const welcome = {
-  greeting: 'Hey',
-  title: 'React',
-};
-
-function getId(myId){
-  return myId
-}
+const list = [
+  {
+    title: 'React',
+    url: 'https://react.js',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org',
+    author: 'Dan Abramox, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  }
+]
 
 function App(){
   return (
     <div>
-      <h1>{welcome.greeting} {welcome.title}</h1>
-
-      <h2>My Id: {getId(42)}</h2>
+      <h1>My Hacker Stories</h1>
 
       <label htmlFor="search">Search:</label>
       <input id="search" type="text" />
+
+      <hr />
+
+      <ul>
+        {list.map(function (item) {
+          return (
+          <li key={item.objectID}>
+           <span>
+            <a href={item.url}>{item.title}</a>
+           </span>
+           <span>{item.author}</span>
+           <span>{item.num_comments}</span>
+           <span>{item.points}</span>
+          </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
