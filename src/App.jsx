d8f608d63d58c1d9c0b212af8fedf5c4c85b26d2
edlance3 <edlance3@gmail.com>
2022-12-20 11:06:03 -0500

    Road to React - react props

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,29 @@
 import * as React from 'react';
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://react.js',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org',
-    author: 'Dan Abramox, Andrew Clarey',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  }
-]
 
-function List() {
-  return (
+
+const List = (props) => (
+ 
     <ul>
-      {list.map(function (item) {
-        return (
-          <li key={item.objectID}>
+      {props.list.map((item) => (
+                    <Item key={item.objectID} item={item} />
+          ))}
+         </ul>
+      );
+
+       const Item = (props) => (
+            <li>
             <span>
-              <a href={item.url}>{item.title}</a>
+              <a href={props.item.url}>{props.item.title}</a>
             </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
+            <span>{props.item.author}</span>
+            <span>{props.item.num_comments}</span>
+            <span>{props.item.points}</span>
           </li>
         );
-      })}
-    </ul>
-  );
-}
+    
+   
+
 
 const Welcome = () =>{
   return ("Welcome to our App!");
@@ -82,6 +69,26 @@ const AddOne = (count) => {
 
 
 function App() {
+
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://react.js',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org',
+      author: 'Dan Abramox, Andrew Clarey',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    }
+  ]
+
   return (
     <div>
       <h1>My Hacker Stories</h1>
@@ -94,7 +101,7 @@ function App() {
       <hr />
 
      
-     <List />
+     <List list={stories} />
 
      <LogOut />
 
