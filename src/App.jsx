7bc3b800255f357f646f38e6fee43ceff9fb37d5
edlance3 <edlance3@gmail.com>
2022-12-21 05:46:35 -0500

    Road to React - React State

--- src/App.jsx
+++ src/App.jsx
@@ -1,75 +1,7 @@
 import * as React from 'react';
 
-
-
-const List = (props) => (
- 
-    <ul>
-      {props.list.map((item) => (
-                    <Item key={item.objectID} item={item} />
-          ))}
-         </ul>
-      );
-
-       const Item = (props) => (
-            <li>
-            <span>
-              <a href={props.item.url}>{props.item.title}</a>
-            </span>
-            <span>{props.item.author}</span>
-            <span>{props.item.num_comments}</span>
-            <span>{props.item.points}</span>
-          </li>
-        );
-    
-   
-
-
-const Welcome = () =>{
-  return ("Welcome to our App!");
-}
-
-const Search = () => {
-
-  const handleChange = (event) => {
-    console.log(event);  //synthetic event 
-    console.log(event.target.value);  //input HTML element
-  };
-
-  return (
-    <div>
-     <label htmlFor="search">Search Here:</label>
-      <input id="search" type="text" onChange={handleChange} />
-    </div>
-  );
-}
-  
-
-
-function LogOut(){
-  return (
-    <div>
-      alert("Are you sure you want to leave?");
-    </div>
-  );
-}
-
-const AddOne = (count) => {
-  if(count > 0){
-    count = count++;
-  } else {
-    count = 10;
-  }
-  return (
-    <div>
-       'The current count is ' +  count
-    </div>
-  ) 
-}
-
-
-function App() {
-
+const App = () => {
+  console.log('App renders'); 
   const stories = [
     {
       title: 'React',
@@ -92,24 +24,96 @@ function App() {
   return (
     <div>
       <h1>My Hacker Stories</h1>
-     
-   
-     <Welcome />
 
-     
-     <Search />
+
+      <Welcome />
+
+
+      <Search />
       <hr />
 
-     
-     <List list={stories} />
 
-     <LogOut />
+      <List list={stories} />
 
-     <br />
-     <AddOne />
+      <LogOut />
+
+      <br />
+      <AddOne />
 
     </div>
   );
+};
+
+const List = (props) => (
+ 
+  <ul>
+    {props.list.map((item) => (
+      <Item key={item.objectID} item={item} />
+    ))}
+  </ul>
+  
+);
+console.log('List renders'); 
+const Item = (props) => (
+  <li>
+    <span>
+      <a href={props.item.url}>{props.item.title}</a>
+    </span>
+    <span>{props.item.author}</span>
+    <span>{props.item.num_comments}</span>
+    <span>{props.item.points}</span>
+  </li>
+);
+
+
+const Welcome = () => {
+  return ("Welcome to our App!");
+}
+
+const Search = () => {
+  console.log('Search renders');  
+  //searchTerm is current state, setSearchTerm is function to update state (state updater function)
+  const [searchTerm, setSearchTerm] = React.useState('');
+
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+    
+    // console.log(event.target.value);  
+  };
+
+  return (
+    <div>
+      <label htmlFor="search">Search Here:</label>
+      <input id="search" type="text" onChange={handleChange} />
+
+      <p>
+        Searching for <strong>{searchTerm}</strong>
+      </p>
+    </div>
+  );
+}
+
+
+
+function LogOut() {
+  return (
+    <div>
+      alert("Are you sure you want to leave?");
+    </div>
+  );
+}
+
+const AddOne = (count) => {
+  if (count > 0) {
+    count = count++;
+  } else {
+    count = 10;
+  }
+  return (
+    <div>
+      'The current count is ' +  count
+    </div>
+  )
 }
 
 export default App;
